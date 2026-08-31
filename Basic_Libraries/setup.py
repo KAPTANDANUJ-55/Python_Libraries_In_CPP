ee59b6e0276ce24a659472decfbac2c3ad649992
kaptan danuj <kaptandanuj@gmail.com>
2026-08-31 14:43:31 +0000

    Initial Commit

--- Basic_Libraries/setup.py
+++ Basic_Libraries/setup.py
@@ -17,7 +17,7 @@ ext_modules = [
 
 setup(
     name="kaptandanuj-easyhelper",
-    version="0.0.3",
+    version="0.0.5",
     description="Beginner friendly helper library powered by C++",
     ext_modules=ext_modules,
     setup_requires=["pybind11>=2.10.0"],
